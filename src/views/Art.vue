0bbfefcc5f3785c325e5993331656eec43aafde9
Joshua <favrejoshua@yahoo.com>
2021-11-18 23:37:19 -0600

    style the Get Started button, remove link from app comp and add home link to art comp

--- src/views/Art.vue
+++ src/views/Art.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="art">
+    <router-link to="/">Home</router-link>
     <h1>Create Your Masterpiece</h1>
     <canvas id="theCanvas"></canvas>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,3 +14,21 @@ export default {
   
 };
 </script>
+
+<style scoped>
+img {
+  width: 7%;
+  height: auto;
+}
+
+button {
+  margin: 1em;
+  padding: 1em 2em;
+  border-radius: 5px;
+  font-size: 1em;
+  font-weight: bold;
+  background-color: rgb(103, 158, 241);
+  box-shadow: 0.2em 0.2em 0.2em rgb(133, 154, 192);
+  cursor: pointer;
+}
+</style>
