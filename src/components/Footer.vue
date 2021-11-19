b718d1cdd09d208b1a89e5e878d911fa7e209d50
Joshua <favrejoshua@yahoo.com>
2021-11-18 23:15:00 -0600

    add new icons, add button to home and change nav links

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="footer">
+    <h3>Footer</h3>
+  </div>
+</template>
+
+<script>
+</script>
+
+
+<style scoped>
+.footer{
+  position: absolute;
+  bottom: 1rem;
+  width: 100%;
+  height: 2.5rem;
+}
+</style>
---
+++ src/views/Art.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="art">
+    <h1>Create Your Masterpiece</h1>
+    <canvas id="theCanvas"></canvas>
+  </div>
+</template>
+
+<style scoped>
+#theCanvas {
+  width: 80%;
+  height: auto;
+  border: 1em solid black
+}
+</style>
